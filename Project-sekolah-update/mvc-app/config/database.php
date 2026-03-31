2cc59c1a2f5f810b974d8ee4498907d007cedee2
Fahmi-sys942 <fumarsyarif@gmail.com>
2026-03-31 14:57:07 +0700

    Create database.php config file

---
+++ Project-sekolah-update/mvc-app/config/database.php
@@ -0,0 +1,1 @@
+<?php $host = 'localhost'; $db_name = 'project_sekolah'; $db_user = 'root'; $db_pass = ''; try { $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_pass); $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); } catch (PDOException $e) { die("Koneksi database gagal: " . $e->getMessage()); } ?>
